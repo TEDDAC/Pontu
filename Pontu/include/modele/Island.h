3f5526b91a2f6c266920cfc6918a3394375d61b3
jathomas2 <jacques.thomas@etu.uca.fr>
2021-11-29 09:53:27 +0100

    Add in model: Island.h and Piece.h

---
+++ Pontu/include/modele/Island.h
@@ -0,0 +1,22 @@
+/**
+ *\file Island.h
+ *\brief Island's model       
+ *\author Jacques
+ *\date 29/11/2021 
+ */
+
+#ifndef ISLAND_H
+#define ISLAND_H
+
+/**
+ *\struct  Island
+ *\brief Island representation 
+ * Island Coordinate on the game board
+ */ 
+typedef struct
+{
+	int x;///< Coordinate : absciss
+	int y;///< Coordinate : ordinate
+}Island;
+
+#endif 
---
+++ Pontu/include/modele/Piece.h
@@ -0,0 +1,26 @@
+/**
+ *\file Piece.h
+ *\brief Piece's model       
+ *\author Jacques
+ *\date 29/11/2021 
+ */
+
+#ifndef PIECE_H
+#define PIECE_H
+#include "Island.h"
+//#include "Player.h" //If we use a pointer for the player instead of an int 
+
+/**
+ *\Struct Piece
+ *\brief Piece representation 
+ *\ Piece characteristics 
+ */
+typedef struct 
+{
+	int idJ; ///< Player's id (owner of the piece)
+	bool stuck; ///< Evaluate to true if the piece is isolated
+	Island island; ///< Piece's localisation on the game board
+
+}Piece;
+
+#endif // PIECE_H
