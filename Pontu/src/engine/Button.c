b224ad1d5f0da4078757bae9d3577f96e23e8b12
thmaillarb <theotime.maillarbaux@etu.uca.fr>
2021-11-29 15:11:29 +0000

    Switched Button's documentation to Doxygen

--- Pontu/src/engine/Button.c
+++ Pontu/src/engine/Button.c
@@ -2,20 +2,6 @@
 #include <stdio.h>
 #include <assert.h>
 
-/*
- * Return : 	a button within written <text>, placed in (<coordx>; <coordy>), sized
- *<sizex>x<sizey>. The button returned will do <onClick> when it will be clicked If text and texture
- *are not define, the function will return NULL and an error is printed in STDERR.
- *
- * Params :	texture	=> background image for the button (can be NULL)
- * 		coordx	=> placement in width
- *		coordy	=> placement in height
- *		sizex	=> button's width
- *		sizey	=> button's height
- *		onClick	=> Acction to do when the button is clicked
- *
- * Finality :	Create a brand new button with specs
- */
 P_Button createButton(SDL_Texture* texture, int coordx, int coordy, int sizex, int sizey, void (*onClick)(void))
 {
 	// Declarations
