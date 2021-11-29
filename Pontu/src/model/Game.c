9b80275cbbe32067d66d6bbffbcde62a4a896a84
thmaillarb <theotime.maillarbaux@etu.uca.fr>
2021-11-29 11:38:32 +0100

    Players are added to the game

--- Pontu/src/model/Game.c
+++ Pontu/src/model/Game.c
@@ -6,8 +6,22 @@ Game newGame(int nbPlayers) {
 	g.currentPlayerID = nbPlayers - 1;
 	g.nb_rounds = 0;
 	g.state = PLACEMENT;
-	// TODO newBoard
-	// TODO initPlayers (avec gestion des couleurs)
+	g.board = newBoard(nbPlayers);
+	
+	// red, green, blue, yellow
+	// TODO meilleures couleurs (?)
+	SDL_Color colors[4] {
+		{255,0  ,0  ,255},
+		{0  ,255,0  ,255},
+		{0  ,0  ,255,255},
+		{255,255,0  ,255}
+	};
+
+	// TODO récup pseudos
+	for (int player_i = 0; player_i < nbPlayers; player_i++) {
+		g.arrPlayers[player_i] = newPlayer("Pseudo",colors[player_i]);
+	}
+	
 	return g;
 }
 
