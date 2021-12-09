9cfebbe9208cf96e71bfae7acde00f4373f5bfd6
Mathis Ribémont <mathis.ribemont@etu.uca.fr>
2021-12-09 09:21:59 +0100

    comment of function

--- Pontu/include/engine/Button.h
+++ Pontu/include/engine/Button.h
@@ -23,7 +23,7 @@ typedef struct
 
 /**
  * \brief Creates a brand new button with specs
- * \param[in]   texture background image for the button (can be NULL)
+ * \param[in]   texture background image for the button (NOT NULL)
  * \param[in]   coordx  placement in width
  * \param[in]   coordy  placement in height
  * \param[in]   sizex   button's width
@@ -33,10 +33,24 @@ typedef struct
  * \return a button created with the specs passed as params, or NULL if there was an error.
  */
 P_Button createButton(SDL_Texture* texture, const int coordx, const int coordy, const int sizex, const int sizey, void (*onClick)(void));
-// texture: design du bouton, si rien n'est passer, le bouton sera générer dans la fonction.
 
-void changeButtonTexture(P_Button* button, const SDL_Texture* texture);
+/**
+ * \brief draw a button on a renderer
+ * \param[in]   renderer	the renderer you want to draw the button on
+ * \param[in]   button	the button you want to draw
+ * \pre At least text or texture must be defined, or an error will be printed in STDERR.
+ * \return SDL_FALSE if the function failed, else SDL_TRUE
+ */
 SDL_bool drawButtonOnRenderer(SDL_Renderer* renderer, const P_Button* button);
+
+/**
+ * \brief Test if a point is on a button
+ * \param[in]   button	the button target
+ * \param[in]   x	x of the point
+ * \param[in]   y	y of the point
+ * \return SDL_TRUE if the point is on the button
+ */
 SDL_bool isHover(P_Button button, int x,int y);// dit si le bouton est survolé en donnant les coordonnées x,y
+//void changeButtonTexture(P_Button* button, const SDL_Texture* texture);
 
 #endif
