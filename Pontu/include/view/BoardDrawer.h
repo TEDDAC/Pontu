319f0252de1e744c036c7c2d70fabc9e88e7edaf
marouault <martin.rouault@etu.uca.fr>
2021-12-16 16:54:29 +0100

    Documented BoardDrawer.h

--- Pontu/include/view/BoardDrawer.h
+++ Pontu/include/view/BoardDrawer.h
@@ -1,3 +1,10 @@
+/**
+ * \file BoardDrawer.h
+ * \brief Define functions to draw the game
+ * \author Martin Rouault
+ * \date 16/12/2021
+ */
+
 #ifndef BOARD_DRAWER_INCLUDED
 #define BOARD_DRAWER_INCLUDED
 
@@ -5,6 +12,17 @@
 #include <stdbool.h>
 #include "model/Board.h"
 
+/**
+ * \brief Draw the board (water, islands and bridges)
+ * 
+ * \param renderer The renderer
+ * \param boardRect The rect were the board is drawn
+ * \param board The game's board (for bridges)
+ * \param island Texture for islands
+ * \param bridge Texture for bridges
+ * \param water Texture for water
+ * \return true I don't know what to return
+ */
 bool drawBoard(SDL_Renderer* renderer, const SDL_Rect* boardRect, const Board* board, SDL_Texture* island, SDL_Texture* bridge, SDL_Texture* water);
 
 
