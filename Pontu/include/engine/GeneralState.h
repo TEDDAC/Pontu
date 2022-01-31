425e7bcacdf0893d1479d3b1616183bdf3043ef4
marouault <martin.rouault@etu.uca.fr>
2022-01-31 11:05:54 +0100

    Add general state

---
+++ Pontu/include/engine/GeneralState.h
@@ -0,0 +1,6 @@
+#ifndef GENERAL_STATE_INCLUDED
+#define GENERAL_STATE_INCLUDED
+
+typedef enum {GS_MainMenu, GS_GameCreationMenu, GS_Game, GS_EndOfGameMenu} GeneralState;
+
+#endif //GENERAL_STATE_INCLUDED
