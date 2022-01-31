be2734a7935031b1539acb81dafc7f080c4426ab
Mathis Ribémont <mathis.ribemont@etu.uca.fr>
2022-01-31 11:40:51 +0100

    main de base

---
+++ Pontu/entryPoints/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "engine/GeneralState.h"
+
+int main(int argc, char const *argv[]) {
+    GeneralState generalState = GS_MainMenu;
+
+    while(generalState != GS_Quit){
+        
+    }
+
+    return 0;
+}
