c2d6e2b10edc1679866abaf03c5030ae70c035e6
jathomas2 <jacques.thomas@etu.uca.fr>
2022-02-28 09:41:58 +0100

    Add the muteSound's ButtonAction, which mute master volume in the game interface

--- Pontu/src/engine/ButtonActions.c
+++ Pontu/src/engine/ButtonActions.c
@@ -1,5 +1,6 @@
 #include "engine/ButtonActions.h"
 #include "engine/GeneralState.h"
+#include "engine/AudioHandler.h"
 
 
 void action_none(P_Button* caller) {
@@ -13,4 +14,10 @@ void action_print(P_Button* caller) {
 void action_setStateToMainMenu(P_Button* caller)
 {
 	*((GeneralState*)caller->arg) = GS_MainMenu;
+}
+
+void action_muteSound(P_Button* caller)
+{
+	//Mute sound -> Master Volume = 0
+	changeMasterVol((AudioHandler*)caller->arg,0);
 }
